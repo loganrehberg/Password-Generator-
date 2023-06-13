cda9b65933050061303d596a09ee0d57e021a57c
loganrehberg <loganrehberg7@gmail.com>
2023-06-13 19:03:33 -0400

    Fixed return value and finished code poggers

--- assets/script.js
+++ assets/script.js
@@ -10,50 +10,57 @@ function generatePassword() {
   );
   // parseInt = turns a string into an interger
   length = parseInt(length);
-  return "Generated password here";
-}
+  
 // NaN = Not a Number
-if (isNaN(length) || length < 8 || length > 128) {
-  alert("Invalid Password Length. Enter a number between 8 and 128");
-  return;
-}
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Invalid Password Length. Enter a number between 8 and 128");
+    return;
+  }
 
-const includeLowercase = confirm("Include lowercase characters?");
-const includeUppercase = confirm("Include uppercase characters?");
-const includeNumeric = confirm("Include numeric characters?");
-var includeSpecial = confirm("Include special characters?");
 
-if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial){
-  alert("Please select at least one character to type");
-  return;
-}
+  const includeLowercase = confirm("Include lowercase characters?");
+  const includeUppercase = confirm("Include uppercase characters?");
+  const includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
 
-const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numericChars = "0123456789";
-const specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial){
+    alert("Please select at least one character to type");
+    return;
+  }
 
-var availableChars = "";
-if (lowercaseChars) {
-  availableChars += lowercaseChars;
-}
-if (uppercaseChars) {
-  availableChars += uppercaseChars;
-}
-if (numericChars) {
-  availableChars += numericChars;
-}
-if (specialChars) {
-  availableChars += specialChars;
-}
 
-let password = "";
-for (let i = 0; i < length; i++) {
-  const randomIndex = Math.floor(Math.random() * availableChars.length);
-  password += availableChars[randomIndex];
-}
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numericChars = "0123456789";
+  const specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+  var availableChars = "";
+  if (lowercaseChars) {
+    availableChars += lowercaseChars;
+  }
+
+  if (uppercaseChars) {
+    availableChars += uppercaseChars;
+  }
+
+  if (numericChars) {
+    availableChars += numericChars;
+  }
+
+  if (specialChars) {
+    availableChars += specialChars;
+  }
+
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * availableChars.length);
+    password += availableChars[randomIndex];
+  }
+
 
 return password;
+}
 
 // prompt("How long would you like your password to be?");
 // confirm("Would you like there to be special characters, lowercase, uppercase, or numeric values? ")
