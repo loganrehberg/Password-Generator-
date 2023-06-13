8d98a1ba58c0e6842294619e881bb4eed8fd6b79
loganrehberg <loganrehberg7@gmail.com>
2023-06-13 15:52:09 -0400

    Added function generatePassword

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,19 @@
+// used https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt for prompt window
+// used https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm for confirm window
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword();
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
