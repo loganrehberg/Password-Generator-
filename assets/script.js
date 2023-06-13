96f47f4ed651fb4edb81f5b54a7c9682297fac55
loganrehberg <loganrehberg7@gmail.com>
2023-06-13 18:55:27 -0400

    Added const and bulk of the JS, troubleshooting now :D

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,70 @@
 // used https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt for prompt window
 // used https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm for confirm window
-// https://www.youtube.com/watch?v=x4HUaiazDes used to point myself in the right direction 
+// https://www.youtube.com/watch?v=x4HUaiazDes used to point myself in the right direction
 //Assignment Code
 var generateBtn = document.querySelector("#generate");
-// Prompt user for password 
-// Length 8 to 128
-// included lowercase, uppercase, numbers, special characters|
-// Validate the input
-// Generate password based on criteria
-
 
 function generatePassword() {
-  var length = prompt("Enter the length of the password (between 8 and 128 characters):");
-  length=parseInt(length)
-  return "Generated password here"
+  var length = prompt(
+    "Enter the length of the password (between 8 and 128 characters):"
+  );
+  // parseInt = turns a string into an interger
+  length = parseInt(length);
+  return "Generated password here";
 }
-prompt("How long would you like your password to be?");
-confirm("Would you like there to be special characters, lowercase, uppercase, or numeric values? ")
+// NaN = Not a Number
+if (isNaN(length) || length < 8 || length > 128) {
+  alert("Invalid Password Length. Enter a number between 8 and 128");
+  return;
+}
+
+const includeLowercase = confirm("Include lowercase characters?");
+const includeUppercase = confirm("Include uppercase characters?");
+const includeNumeric = confirm("Include numeric characters?");
+var includeSpecial = confirm("Include special characters?");
+
+if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial){
+  alert("Please select at least one character to type");
+  return;
+}
+
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numericChars = "0123456789";
+const specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+var availableChars = "";
+if (lowercaseChars) {
+  availableChars += lowercaseChars;
+}
+if (uppercaseChars) {
+  availableChars += uppercaseChars;
+}
+if (numericChars) {
+  availableChars += numericChars;
+}
+if (specialChars) {
+  availableChars += specialChars;
+}
+
+let password = "";
+for (let i = 0; i < length; i++) {
+  const randomIndex = Math.floor(Math.random() * availableChars.length);
+  password += availableChars[randomIndex];
+}
+
+return password;
+
+// prompt("How long would you like your password to be?");
+// confirm("Would you like there to be special characters, lowercase, uppercase, or numeric values? ")
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
 
   passwordText.value = password;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
